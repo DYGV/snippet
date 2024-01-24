e7765fd38950fd84624ff805edaa5fe33e7abf58
dygv <11eisuke88@gmail.com>
2024-01-24 13:36:41 +0900

    change comments

--- logger-fpga-alveo-thermal-electrical.sh
+++ logger-fpga-alveo-thermal-electrical.sh
@@ -2,6 +2,7 @@
 
 # This script is used to log the FPGA temperature and power usage of the Alveo card.
 # This script is run on the host machine and uses xbutil to get the temperature and power usage.
+# "-d 0000:01:00.1" is the PCIe bus number of the Alveo card. This can be found using lspci -v.
 
 count=0
 while true
