a542f01cd053850d19aa6f90dbf6e1d4fc2a355d
DYGV <11eisuke88@gmail.com>
2024-06-07 01:21:09 +0900

    refine

--- logger-fpga-alveo-thermal-electrical.sh
+++ logger-fpga-alveo-thermal-electrical.sh
@@ -9,7 +9,8 @@ while true
 do
   count=`expr $count + 1`
   date=`date +%Y-%m-%d-%H:%M:%S.%3N`
-  fpga_temp=$(/opt/xilinx/xrt/bin/xbutil examine -r thermal electrical -d  0000:01:00.1  | grep 'FPGA' |  awk '{print $3}' |  sed -e 's/[^0-9]//g' | tr -d '\n')
-  power_usage=$(/opt/xilinx/xrt/bin/xbutil examine -r thermal electrical -d  0000:01:00.1 | grep  'Power' | awk '{print $3}' | tr -d '\n' |  sed -e 's/[^0-9.]//g')
-  echo "$count,$date,$fpga_temp,$power_usage"
+  examine=$(/opt/xilinx/xrt/bin/xbutil examine -r thermal electrical -d  0000:01:00.1)
+  fpga_temp=$(echo "$examine" | grep 'FPGA' |awk '{print $3}' |  sed -e 's/[^0-9]//g' | tr -d '\n')
+  power_usage=$(echo "$examine" | grep 'Power' |awk '{print $3}' | tr -d '\n' |  sed -e 's/[^0-9.]//g')
+  printf "$date, $fpga_temp C, %.1f W\n" "$power_usage"
 done
